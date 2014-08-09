96ec8817cc5d8e97dde06a8a5751fbca6d4168f5
Will Jaspers <wjaspers4@gmail.com>
2014-08-09 18:18:27 +0000

    Split exceptions into appropriate type(s).
    ErrorExceptions handle exceptions thrown on behalf of error(s).
    The original HealthCheckException can be used by tests or other services to indicate related problems.

--- src/TestsAlwaysIncluded/HealthCheck/Exception/HealthCheckException.php
+++ src/TestsAlwaysIncluded/HealthCheck/Exception/HealthCheckException.php
@@ -2,33 +2,6 @@
 
 namespace TestsAlwaysIncluded\HealthCheck\Exception;
 
-class HealthCheckException extends \Exception
+class HealthCheckException extends \Exception implements HealthCheckExceptionInterface
 {
-
-    /**
-     * @param \string $message
-     * @param \int $code
-     * @param Exception $previous
-     */
-    public function __construct($message = null, $code = 0, Exception $previous = null) {
-        parent::__construct($message, $code, $previous);
-    }
-
-
-    /**
-     * @param \string $file
-     */
-    public function setFile($file)
-    {
-        $this->file = $file;
-    }
-
-
-    /**
-     * @param \int $line
-     */
-    public function setLine($line)
-    {
-        $this->line = $line;
-    }
 }
