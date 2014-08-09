8f7cec3a218968d8a9598108c94bdf039aa3db66
Will Jaspers <wjaspers4@gmail.com>
2014-08-09 06:50:54 +0000

    Create a statistics reporter.

---
+++ src/TestsAlwaysIncluded/HealthCheck/Reporter/TestStatisticsReporter.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace TestsAlwaysIncluded\HealthCheck\Reporter;
+
+use TestsAlwaysIncluded\HealthCheck\Event\HealthCheckEvent;
+
+class TestStatisticsReporter extends Reporter
+{
+    /** @var int */
+    protected $failed = 0;
+
+    /** @var int */
+    protected $passed = 0;
+
+    /** @var int */
+    protected $errors = 0;
+
+    /** @var int */
+    protected $skipped = 0;
+
+
+    /**
+     * @return int
+     */
+    public function getFailed()
+    {
+        return $this->failed;
+    }
+
+
+    /**
+     * @return int
+     */
+    public function getPassed()
+    {
+        return $this->passed;
+    }
+
+
+    /**
+     * @return int
+     */
+    public function getErrors()
+    {
+        return $this->errors;
+    }
+
+
+    /**
+     * @return int
+     */
+    public function getSkipped()
+    {
+        return $this->failed;
+    }
+
+
+    /**
+     * @param HealthCheckEvent $healthCheckEvent
+     */
+    public function testFailed(HealthCheckEvent $event)
+    {
+        $this->failed ++;
+    }
+
+
+    /**
+     * @param HealthCheckEvent $healthCheckEvent
+     */
+    public function testPassed(HealthCheckEvent $event)
+    {
+        $this->passed ++;
+    }
+
+
+    /**
+     * @param HealthCheckEvent $healthCheckEvent
+     */
+    public function testError(HealthCheckEvent $event)
+    {
+        $this->errors ++;
+    }
+
+
+    /**
+     * @param HealthCheckEvent $healthCheckEvent
+     */
+    public function testSkipped(HealthCheckEvent $event)
+    {
+        $this->skipped ++;
+    }
+}
