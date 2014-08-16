0f3ad7bc375933741cac9d9533ee9e64dc0595f3
Will Jaspers <wjaspers4@gmail.com>
2014-08-16 03:34:32 +0000

    Remove run() from the HealthCheck.
    Now that each HealthCheck can be accessed from the HealthCheckEvent, we do not want it to be possible to execute run() again.

--- src/TestsAlwaysIncluded/HealthCheck/Service/HealthCheck.php
+++ src/TestsAlwaysIncluded/HealthCheck/Service/HealthCheck.php
@@ -3,32 +3,17 @@
 namespace TestsAlwaysIncluded\HealthCheck\Services;
 
 use Symfony\Component\EventDispatcher\EventDispatcher;
-use TestsAlwaysIncluded\HealthCheck\Exception\HealthCheckExceptionInterface;
-use TestsAlwaysIncluded\HealthCheck\HealthCheckEvents;
 use TestsAlwaysIncluded\HealthCheck\Reporter\Reporter;
 use TestsAlwaysIncluded\HealthCheck\Test\TestSuite;
 
 class HealthCheck
 {
-    /** @var EventDispatcher */
-    protected $eventDispatcher;
-
-
     /** @var TestSuite[] */
     protected $testSuites = array();
 
 
     /** @var Reporter[] */
     protected $reporters = array();
-
-
-    /**
-     * @param EventDispatcher $eventDispatcher
-     */
-    public function setEventDispatcher(EventDispatcher $eventDispatcher)
-    {
-        $this->eventDispatcher = $eventDispatcher;
-    }
 
 
     /**
@@ -54,7 +39,7 @@ class HealthCheck
     /**
      * @param TestSuite $testSuite
      */
-    public function registerTestSuite(TestSuite $testSuite)
+    public function addTestSuite(TestSuite $testSuite)
     {
         $this->testSuites[] = $testSuite;
     }
@@ -66,67 +51,5 @@ class HealthCheck
     public function getTestSuites()
     {
         return $this->testSuites;
-    }
-
-
-    /**
-     * @param string $eventName
-     * @param TestSuite $testSuite
-     * @param TestGroup $testGroup
-     * @param Test $test
-     */
-    private function fire($eventName, TestSuite $testSuite = null, TestGroup $testGroup = null, Test $test = null)
-    {
-        $event = new HealthCheckEvent();
-        $event->setTestSuite($testSuite);
-        $event->setTestGroup($testGroup);
-        $event->setTest($test); 
-        $this->eventDispatcher->dispatch($eventName, $event);
-    }
-
-
-    /**
-     * Executes the HealthCheck.
-     */
-    public function run()
-    {
-        $this->fire(HealthCheckEvent::EVENT_HEALTH_CHECK_STARTED);
-
-        foreach ($this->testSuites as $testSuite) {
-            $this->fire(HealthCheckEvent::EVENT_TEST_SUITE_STARTED, $testSuite);
-
-            foreach ($testSuite->getTestGroups() as $testGroup) {
-                $this->fire(HealthCheckEvent::EVENT_TEST_GROUP_STARTED, $testSuite, $testGroup);
-
-                foreach ($group->getTests() as $test) {
-                    $this->fire(HealthCheckEvent::EVENT_TEST_STARTED, $testSuite, $testGroup, $test);
-
-                    try {
-                        $test->execute();
-                    } catch (HealthCheckExceptionInterface $exception) {
-                        $test->error($exception->getMessage());
-                    }
-
-                    if ($test->passed()) {
-                        $eventName = HealthCheckEvent::EVENT_TEST_PASSED;
-                    } else if ($test->failed()) {
-                        $eventName = HealthCheckEvent::EVENT_TEST_FAILED;
-                    } else if ($test->inError()) {
-                        $eventName = HealthCheckEvent::EVENT_TEST_ERROR;
-                    } else if ($test->skipped()) {
-                        $eventName = HealthCheckEvent::EVENT_TEST_SKIPPED;
-                    }
-
-                    $this->fire($eventName, $testSuite, $testGroup, $test);
-                    $this->fire(HealthCheckEvent::EVENT_TEST_COMPLETED, $testSuite, $testGroup, $test);
-                }
-
-                $this->fire(HealthCheckEvent::EVENT_TEST_GROUP_COMPLETED, $testSuite, $testGroup);
-            }
-
-            $this->fire(HealthCheckEvent::EVENT_TEST_SUITE_COMPLETED, $testSuite);
-        }
-
-        $this->fire(HealthCheckEvent::EVENT_HEALTH_CHECK_COMPLETED);
     }
 }
